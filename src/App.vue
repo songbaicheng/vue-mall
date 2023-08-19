a1b247f3380948786113795117ce82583971ac5e
songbaicheng <2524218694@qq.com>
2023-08-19 13:04:17 +0800

    add home page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router'
-</script>
-
 <template>
   <!-- 路由出口，路由匹配到的组件将渲染在这里 -->
   <router-view></router-view>
 </template>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
