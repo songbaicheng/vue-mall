fef82de795d1a2ff98adf08bf4e3946feeed77d0
songbaicheng <2524218694@qq.com>
2023-08-20 23:06:39 +0800

    delete template error code

--- src/components/layout/BaseHeader.vue
+++ src/components/layout/BaseHeader.vue
@@ -1,7 +1,3 @@
 <template>
-    <el-page-header @back="goBack">
-        <template #content>
-            <span class="text-large font-600 mr-3"> Title </span>
-        </template>
-    </el-page-header>
+
 </template>
