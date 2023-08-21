61c3baeab0f3a4f6a62aa883f79374ae7b803867
songbaicheng <2524218694@qq.com>
2023-08-21 15:55:48 +0800

    modify login page

--- src/components/layout/BaseHeader.vue
+++ src/components/layout/BaseHeader.vue
@@ -1,3 +1,20 @@
 <template>
-
+    <div class="mobile-header">
+    </div>
 </template>
+
+<style lang="scss" scoped>
+.mobile-header {
+    background: rgba(255, 255, 255, 0.65);
+    box-shadow: 0 2px 10px 0 rgba(0, 0, 0, 0.2);
+    padding: 10px;
+    text-align: left;
+    z-index: 999;
+
+    .caidan {
+        margin-left: 10px;
+        font-size: 10px;
+        cursor: pointer;
+    }
+}
+</style>
