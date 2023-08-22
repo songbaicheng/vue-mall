623903cb063a32a1ca64592c6bd51abcfe9491b3
songbaicheng <2524218694@qq.com>
2023-08-22 11:16:00 +0800

    beauty login page

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="login-backgroud">
-        <LoginForm></LoginForm>
+        <div class="login-wrapper">
+            <div class="login-container">
+                <LoginForm></LoginForm>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -10,4 +14,21 @@ import LoginForm from '~/components/login/LoginForm.vue';
 
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.login-backgroud {
+    height: 100vh;
+}
+
+.login-wrapper {
+    padding-top: 8%;
+    height: 100vh;
+    width: 80vh;
+    margin: 0 auto;
+}
+
+.login-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
