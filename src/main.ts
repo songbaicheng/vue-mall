8b550f773cfdb1ade1ef1f2a79da50bba25a7ce6
songbaicheng <2524218694@qq.com>
2023-08-28 00:00:06 +0800

    delete favicon and modify annoation

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
 import { createApp } from "vue";
-import App from "~/App.vue"; 
+import App from "~/App.vue";
 
-import ElementPlus from 'element-plus' // Element-plues
-import router from '~/router' // vue-router
-import * as ElementPlusIconsVue from '@element-plus/icons-vue' // icon 图标
+import ElementPlus from 'element-plus'
+import router from '~/router'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import "~/styles/index.scss"; // 引入自定义 scss
-import zhCn from 'element-plus/es/locale/lang/zh-cn'; // 国际化-中文
-import { createPinia } from 'pinia' // pinia
+import zhCn from 'element-plus/es/locale/lang/zh-cn';
+import { createPinia } from 'pinia'
 
 const app = createApp(App);
-app.use(router)
-app.use(ElementPlus, {
-  locale: zhCn
+app.use(router) // vue-router
+app.use(ElementPlus, { // Element-plues
+  locale: zhCn // 国际化-中文
 });
-app.use(createPinia())
+app.use(createPinia()) // pinia
 app.mount("#app");
 
 // 全局注册 Element Plus 常用 icon 图标
