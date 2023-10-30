70b2c9084ae771d0b0ef4a263de8dc1a743e9c6f
songbaicheng <2524218694@qq.com>
2023-10-30 18:05:30 +0800

    beauty login page

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,8 +1,15 @@
 <template>
     <div class="login-backgroud">
         <div class="login-wrapper">
-            <div class="login-container">
-                <LoginForm></LoginForm>
+            <div :class="screenWidth > 1110 ? 'left inline-block' : ''">
+                <div class="login-title">
+                    <span>使用 Vue3 + Element Plus创建的商城后台</span>
+                </div>
+            </div>
+            <div :class="screenWidth > 1110 ? 'right inline-block' : ''">
+                <div class="login-container">
+                    <LoginForm></LoginForm>
+                </div>
             </div>
         </div>
     </div>
@@ -12,23 +19,92 @@
 import { ref, onMounted } from 'vue';
 import LoginForm from '~/components/login/LoginForm.vue';
 
+const globalStore = GlobalStore();
+const screenWidth = ref(null);
+const getStatus = async () => {
+    const res = await checkIsSafety(globalStore.entrance);
+    if (res.data === 'unpass') {
+        router.replace({ name: 'entrance' });
+    }
+};
+
+onMounted(() => {
+    getStatus();
+    screenWidth.value = document.body.clientWidth;
+    window.onresize = () => {
+        return (() => {
+            screenWidth.value = document.body.clientWidth;
+        })();
+    };
+});
 </script>
 
 <style scoped lang="scss">
 .login-backgroud {
     height: 100vh;
-}
+    background-image: url("~/assets/images/login_backgroud.png");
+    background-size: scale-down;
 
-.login-wrapper {
-    padding-top: 8%;
-    height: 100vh;
-    width: 80vh;
-    margin: 0 auto;
-}
+    .login-wrapper {
+        padding-top: 8%;
+        width: 80%;
+        margin: 0 auto;
 
-.login-container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
+        .left {
+            vertical-align: middle;
+            text-align: right;
+            width: 60%;
+            img {
+                object-fit: contain;
+                width: 100%;
+                @media only screen and (min-width: 1440px) {
+                    width: 85%;
+                }
+            }
+        }
+        .right {
+            vertical-align: middle;
+            width: 40%;
+        }
+    }
+
+    .login-title {
+        text-align: right;
+        margin-right: 10%;
+        span:first-child {
+            color: #2378d2;
+            font-size: 40px;
+            font-family: pingFangSC-Regular;
+            font-weight: 600;
+            @media only screen and (max-width: 768px) {
+                font-size: 35px;
+            }
+        }
+        @media only screen and (max-width: 1110px) {
+            margin-bottom: 20px;
+            font-size: 35px;
+            text-align: center;
+            margin-right: 0;
+        }
+    }
+    .login-container {
+        margin-top: 40px;
+        padding: 40px 0;
+        width: 390px;
+        box-sizing: border-box;
+        background-color: rgba(255, 255, 255, 0.55);
+        border-radius: 4px;
+        box-shadow: 2px 4px 22px rgba(0, 94, 235, 0.2);
+        @media only screen and (max-width: 1440px) {
+            margin-top: 60px;
+        }
+        @media only screen and (max-width: 1110px) {
+            margin: 60px auto 0;
+        }
+
+        @media only screen and (max-width: 768px) {
+            width: 100%;
+        }
+    }
 }
 </style>
